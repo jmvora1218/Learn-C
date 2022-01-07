5daf605abbac9cf4a834bf963c2e696cacd26def
Jay  Vora <jmvora1218@gmail.com>
2022-01-07 14:33:41 +0530

    Git Scrip commit

--- ex17.c
+++ ex17.c
@@ -46,4 +46,83 @@ void Database_load(struct Conection *conn)
 		die("Failed to load Database.");
 }
 
+struct Connection *Database_open(const char *filename, char mode)
+{
+	struct Connection *conn = malloc(sizeof(struct Connection));
+	if(!conn)
+		die("Memory error");
 
+	conn->db = malloc(sizeof(struct Databse));
+	if(!conn->db)
+		die("Memory error");
+
+	if(mode == 'c'){
+		conn->file = fopen(filename, "w");
+	}else{
+		conn->file =fopen (filename, "r+");
+		
+		if(conn->file){
+			Databse_load(conn);
+		}
+	}
+
+	if(!conn->file)
+		die("Failed to open the file");
+	
+	return conn;
+}
+
+void Databse_close(struct Connection *conn)
+{
+	if(conn){
+		if(conn->file)
+			fclose()conn->file;
+		if(conn->db)
+			free(conn-db);
+		free(conn);
+	}
+}
+
+void Database_write(struct Connection *conn)
+{
+	rewind(conn->file);
+
+	int rc = fwrite(conn->db, sizeod(struct Database), 1, conn->file);
+	if(rc !=1)
+		die("Failed to write database.");
+
+	rc =fflush(conn->file);
+	if(rc== -1)
+		die("Can not flush database");
+}
+
+void Database_create(struct Connection *conn)
+{
+	int i=0;
+
+	for(i=0;i<MAX_ROWS;i++){
+	//make  a prototype to initialize it
+	struct Address addr = (.id=i,.set=0);
+	//then just assign it
+	conn->db->rows[i] =addr;
+	}
+}
+
+void Database_set(struct Connection *conn, int id, const char *name, const char *email)
+{
+	struct Address *addr = &con->db->rows[id];
+	if(addr->set)
+		die("Already set, delete it first");
+	
+	addr->set ==1;
+//WARNING: bug,read the "How to break IT" and fix this
+	char *res=strncpy(addr->name, name, MAX_DATA);
+//demonstrate the strncpy bug
+	if(!res)
+		die("Name copy failed");
+	
+	res = strncpy(addr->email, email, MAX_DATA)
+	if(!res)
+		die("Email copy failed");
+	
+}

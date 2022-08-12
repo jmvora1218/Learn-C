3bfa3d0b48b92515e8f65dd5a27ef524836accec
Jay  Vora <jmvora1218@gmail.com>
2022-08-12 10:40:13 +0530

    Git Scrip commit

--- Makefile
+++ Makefile
@@ -1,6 +1,1 @@
 CFLAGS=-Wall -g
-
-clean: 
-	rm -f ex08
-	rm -f ex16 
-	rm -f ex17
--- dbg.h
+++ dbg.h
@@ -1,6 +1,5 @@
-#ifdef NDEBUG
-#ifndef __ex19_dbg_h__
-#define __ex19_dbg_h__
+#ifndef __dbg_h__
+#define __dbg_h__
 
 #include <stdio.h>
 #include <errno.h>
@@ -9,12 +8,15 @@
 #ifdef NDEBUG
 #define debug(M, ...)
 #else
-#define debug(M, ...) fprintf(stderr, M "[DEBUG] %s:%d:" "\n",##__VA_ARGS__,__FILE__, __LINE__)
+#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n",\
+        __FILE__, __LINE__, ##__VA_ARGS__)
 #endif
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
-#define log_err(M, ...) fprintf(stderr,"[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__,clean_errno(), ##__VA_ARGS__)
+#define log_err(M, ...) fprintf(stderr,\
+        "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__,\
+        clean_errno(), ##__VA_ARGS__)
 
 #define log_warn(M, ...) fprintf(stderr,\
         "[WARN] (%s:%d: errno: %s) " M "\n",\
